afabea824ecc1192975b4c7354ee4f64f38fa2ca
razcrisan <rcrisan13@gmail.com>
2017-10-11 11:27:46 -0400

    added property and method

--- Week3_Day3INHERITANCE/Vehicle.cs
+++ Week3_Day3INHERITANCE/Vehicle.cs
@@ -8,5 +8,29 @@ namespace Week3_Day3INHERITANCE
 {
     class Vehicle
     {
+        private int seats;
+        private int carryingCapacity;
+        private string color;
+        private double movementSpeed;
+        private float gearRatio;
+        private float engineSize;
+
+        public int Seats { get;}
+
+        public int CarryingCapacity { get; }
+
+        public string Color { get; set; }
+        
+        public Vehicle(int seats, int carryingCapacity, string color, double movementSpeed)
+        {
+            this.seats = seats;
+            this.carryingCapacity = carryingCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+        }
+
+
+
+
     }
 }
